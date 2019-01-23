8c543f7420d7841acf0f20169f9224f928b86d5b
Trey Turner <tturner@liveearth.com>
2019-01-23 11:22:31 -0600

    Change example to show that .getAttribute returns a promise when called from within element command, even within a sync function

--- specs/demo.spec.js
+++ specs/demo.spec.js
@@ -10,17 +10,19 @@ search = require('../pages/search.page');
  * @param {String} string The class to search for
  * @returns {boolean} Whether or not the element had that class
  */
-browser.addCommand('hasClass', function(element, string) {
-  return element.getAttribute('class').split(' ').includes(string);
-});
+browser.addCommand('hasClass', function(string) {
+  let classes = this.getAttribute('class');
+  console.log(`classes: `, classes);
+  return classes.split(' ').includes(string);
+}, true);
 
 before(function() {
   browser.url('/');
   search.input.waitForDisplayed(5000);
 });
 
-describe("custom commands", () => {
-  it("shouldn't output to stderr", () => {
-    expect(browser.hasClass(search.input, 'foo')).to.be.false;
+describe("getAttribute from within a synchronous custom element command", () => {
+  it("shouldn't return a promise", () => {
+    expect(search.input.hasClass('foo')).to.be.false;
   });
 });
